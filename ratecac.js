8feedd431dd55ff402e0a85e62d7ad01e8495dac
BDWar <BDWareing@gmail.com>
2021-03-27 20:46:34 -0600

    first

---
+++ ratecac.js
@@ -0,0 +1,8 @@
+var mailtype = 0;
+var weight = 0;
+var total = weight + mailtype;
+
+function calacuteShipping(price)
+{
+
+}
---
+++ rdata.php
@@ -0,0 +1,19 @@
+<?php
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, retrusrantname, loction, foodtype FROM retrusrant";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. "Name: ". $row["retrusrantname"]. $row["loction"]
+        $row["foodtype"];
+    } else{
+        echo "0 results";
+    }
+    $conn->close();
+}
+?>
